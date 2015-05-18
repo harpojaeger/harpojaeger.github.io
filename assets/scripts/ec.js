b6b3196daa7b5a334d697752ce66cd393de2d5be
Harpo Jaeger <harpo.jaeger@gmail.com>
2015-05-18 16:20:11 -0700

    EC script changes
    Created interval counter to speed changes to completed poem display.

--- assets/scripts/ec.js
+++ assets/scripts/ec.js
@@ -98,9 +98,10 @@ return n + (sfx[(val-20)%10] || sfx[val] || sfx[0]);
 function build_navigation(){
 $.get("http://ec2-54-164-53-69.compute-1.amazonaws.com/exquisite_corpse_assets/completed_poems_counter.php",function(n){
 html="";
-while(n/10!=Math.round(n/10))
+interval=25;
+while(n/10!=Math.round(n/interval))
 n--;
-for(n;n>0;n=n-10){
+for(n;n>0;n=n-interval){
 html+='<a href="#'+n+'">'+n+"<a/> ";
 }
 $("#poem_navigation").html(html);
